1e82225a97e080bb57a4550255d0363630a31fdf
Janith Madarasinghe <janithpm9991@gmail.com>
2022-05-20 00:16:52 +0530

    temporary change

--- client/src/views/Blog/index.js
+++ client/src/views/Blog/index.js
@@ -29,8 +29,10 @@ function Blog() {
   )
 }
 
-export default withAuthenticationRequired(Blog, {
-  // Show a message while the user waits to be redirected to the login page.
-  onRedirecting: () => <div>Redirecting you to the login page...</div>,
-});
+// export default withAuthenticationRequired(Blog, {
+//   // Show a message while the user waits to be redirected to the login page.
+//   onRedirecting: () => <div>Redirecting you to the login page...</div>,
+// });
+
+export default Blog
 
